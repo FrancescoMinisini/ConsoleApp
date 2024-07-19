ed00f6990465920a0d6c413ff50d7795b2b9506d
Francesco Minisini <francesco.minisini@p2000.it>
2024-07-19 08:45:44 +0200

    tesst 1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string string1 = "";
+        string string2 =  "";
+        string output = $"buongiorno {string1} {string2}"; 
+
+        List<int> a = new List<int>();
+
+        int lunghezza = a.Count;
+        var lunghezza2 = a.Count();
+
+        Console.WriteLine($"buongiorno {string1} {string2}");
+    }    
+} 
