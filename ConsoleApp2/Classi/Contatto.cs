a19dd18ab7f61369e752508242f98689dcc14c2b
Francesco Minisini <francesco.minisini@p2000.it>
2024-07-19 17:28:45 +0200

    Inizio progetto

---
+++ ConsoleApp2/Classi/Contatto.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2.Classi
+{
+    internal class Contatto
+    {
+        private string name ;
+        private string surname;
+        private string number;
+        private string email;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Surname { get; set; } = string.Empty;
+
+        public string Number { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+
+        public Contatto( string cognome, string nome , string numero, string mail)
+        {
+            Name = nome;
+            Surname = cognome;
+            Number = numero;
+            Email = mail;
+        }
+        public Contatto()
+        {
+        }
+
+    }
+
+    internal class Rubrica
+    {
+        public List<Contatto> elenco; 
+
+        public void Add( Contatto nuovo){
+           this.elenco.Add(nuovo);
+            OrdinaLista();
+            }
+        public void Add(List<Contatto> nuovo)
+        {
+            this.elenco.AddRange(nuovo);
+            OrdinaLista();
+        }
+
+        public void Delete()
+        {
+            this.elenco.Clear();
+        }
+
+        public void OrdinaLista()
+        {
+            this.elenco = this.elenco.OrderBy(x=> x.Surname).ThenBy(x=> x.Name).ToList();   
+        }
+
+        public Rubrica(List<Contatto> contatti)
+        {
+            this.elenco = contatti;
+        }
+
+        public Rubrica()
+        {
+            this.elenco = new List<Contatto>();
+        }
+
+    }
+
+}
+
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,85 @@
-﻿internal class Program
-{
+﻿using ConsoleApp2.Classi;
+
+internal class Program
+{   
     private static void Main(string[] args)
-    {
-        string string1 = "";
-        string string2 =  "";
-        string output = $"buongiorno {string1} {string2}"; 
+    { 
+        int control = -1;
+        int index = 0;
+        List<Contatto> elenco = new List<Contatto>();
+        Rubrica rubrica = new Rubrica();
 
-        List<int> a = new List<int>();
+        //Inizializzo abdul
+        Contatto Abdul = new Contatto("Elbadwy", "Abdulrhman", "123123", "abdul");
+        rubrica.Add(Abdul);
+        //Console.WriteLine( $" Contatto numero: {index + 1} \n Cognome: {rubrica.elenco[index].Surname} \n Nome: {rubrica.elenco[index].Name} \n Numero di telefono: {rubrica.elenco[index].Number} \n Mail: {rubrica.elenco[index].Email}");
 
-        int lunghezza = a.Count;
-        var lunghezza2 = a.Count();
+        Dictionary<int, string> dic = new Dictionary<int, string>()
+        {
+            {0 , "Chiudi il programma"},
+            {1, "Visualizza i contatti" },
+            {2 ,"Aggiungi un contatto" },
+            {3 , "Rimuovi un contatto" },
+            {4, "Cancella la rubrica" }
+        };
+        List<string> list = new List<string>()
+        {
+            "Cognome" , "Nome" , "#Telefono" , "Email"
+        };
+        
+        Console.WriteLine($"\n{Abdul.GetType()}");
+        Type type = typeof(Contatto);
 
-        Console.WriteLine($"buongiorno {string1} {string2}");
-    }    
-} 
+
+        while (control != 0)
+        {
+            Console.WriteLine("\nPremere: ");
+            foreach (int i in dic.Keys) Console.WriteLine($"\n{dic.Keys} {dic.Values}");
+
+            control = Convert.ToInt32(Console.ReadLine());
+
+            var a = Math.Max(0, control);
+
+            switch (control)
+            {
+                case 1:
+                    {
+                        //per visualizzare i contatti
+                        Console.Clear();
+                        foreach (string field in list) Console.WriteLine($"{field}       ");
+                        foreach (Contatto field in rubrica.elenco)
+                        {
+                            Console.WriteLine($"{field.Surname}    {field.Name}    {field.Number}   {field.Email}");
+                        }
+                        break;
+                    }
+                case 2: {
+                        Contatto temp = new Contatto();
+                        Console.Clear();
+                        Console.WriteLine("\n Inserire Nome:  ");
+                        temp.Name = Console.ReadLine();
+                        Console.WriteLine("\n Inserire Cognome:  ");
+                        temp.Surname = Console.ReadLine();
+                        Console.WriteLine("\n Inserire #Telefono:  ");
+                        temp.Number = Console.ReadLine();
+                        Console.WriteLine("\n Inserire Mail:  ");
+                        temp.Email = Console.ReadLine();
+                        break;
+
+                    }
+                case 3: { break; }
+
+                case 4: { break; }
+
+                default: {
+                        Console.WriteLine("Hai inserito un valore non valido");
+                        break; 
+                    }
+
+            }
+        }
+
+    }
+
+}    
+ 
